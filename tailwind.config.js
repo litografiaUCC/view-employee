337f4d5a68c9488127d962fab24a9085197ad674
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-04-12 19:46:14 +0000

    Fix bug animation loadder

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-          drop: {
-            '0%': { transform: 'translateY(-100%)' },
-            '100%': { transform: 'translateY(0)' },
-          },
-          up: {
-            '0%': { transform: 'translateY(0)' },
-            '100%': { transform: 'translateY(-100%)' },
-          }
-        }
-      },
-      animation: {
-        'drop-once': 'drop 0.5s ease-out',
-        'up-once': 'drop 0.5s ease-out reverse',
-      }
+    
     },
   plugins: [],
 }
